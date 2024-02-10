b29ff5c77b571b49b2cc42133a4ecbd177724958
e542137 <kevin.werner@sbb.ch>
2024-02-10 01:03:19 +0100

    add menubar

--- dynamic-feedback-ui/src/app/app.component.ts
+++ dynamic-feedback-ui/src/app/app.component.ts
@@ -2,11 +2,14 @@ import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {TeamMemberCardComponent} from "./team-member-card/team-member-card.component";
 import {JobLevel, TeamMember} from "./model/models";
+import {MenubarModule} from "primeng/menubar";
+import {MenuItem} from "primeng/api";
+import {ImageModule} from "primeng/image";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TeamMemberCardComponent],
+  imports: [RouterOutlet, TeamMemberCardComponent, MenubarModule, ImageModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -18,4 +21,12 @@ export class AppComponent {
     jobTitle: "Software Engineer",
     jobLevel: JobLevel.SENIOR
   }
+  menuItems: MenuItem[];
+
+  constructor() {
+    this.menuItems = [
+      {}
+    ]
+  }
+
 }
